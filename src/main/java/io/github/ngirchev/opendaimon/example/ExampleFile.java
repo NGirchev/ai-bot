06b7b30c22faf6adfbd30b3ca662a804e4610f68
ngirchev <ngirchev@gmail.com>
2026-03-15 16:01:05 +0200

    Add ExampleFile.java

---
+++ src/main/java/io/github/ngirchev/opendaimon/example/ExampleFile.java
@@ -0,0 +1,7 @@
+package io.github.ngirchev.opendaimon.example;
+
+public class ExampleFile {
+    public static void main(String[] args) {
+        System.out.println("Hello from ExampleFile!");
+    }
+}
